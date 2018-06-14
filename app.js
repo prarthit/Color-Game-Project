b49f27229f4905c68c13c5cc837df31a0a988d51
Prarthit Mehra <32339911+prarthit@users.noreply.github.com>
2018-06-15 00:08:54 +0530

    Easy mode added!

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var colors = [];
+var numSquares = 6;
 var pickedColorIndex = 0;
 var pickedColor;
 var colorDisplay = document.querySelector("#colorDisplay");
+var msg = document.querySelector("#message")
 
 var squares = document.querySelectorAll(".square");
 
 function initialize(){
-	setUpSquares();
 	setUpModebuttons();
-	selectSolution();
+	setUpSquares();
 }
 
 function setUpModebuttons(){
@@ -20,6 +21,8 @@ function setUpModebuttons(){
 			modes[0].classList.remove("selected");
 			modes[1].classList.remove("selected");
 			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			setUpSquares();
 		});		
 	}
 }
@@ -27,15 +30,27 @@ function setUpModebuttons(){
 function setUpSquares(){
 	colors = [];
 	for(var i=0; i<squares.length; i++){
-		colors.push(generateRandomColor());
-		squares[i].style.background = colors[i];
-		squares[i].addEventListener("click",function(){
-			if(this.style.background === pickedColor)
-				changecolor(pickedColor);
-			else
-				this.style.background = "#232323";
-		});
+		if(i>=numSquares)
+		{
+			squares[i].style.display = "none";
+		}
+		else
+		{
+			squares[i].style.display = "block";
+			colors.push(generateRandomColor());
+			squares[i].style.background = colors[i];
+			squares[i].addEventListener("click",function(){
+				if(this.style.background === pickedColor)
+					changecolor(pickedColor);
+				else
+					{
+						this.style.background = "#232323";
+						msg.textContent = "Try Again";
+					}
+			});
+		}
 	};
+	selectSolution();
 }
 
 function changecolor(color){
@@ -43,10 +58,13 @@ function changecolor(color){
 	{
 		squares[i].style.background = color;
 	}
+	(document.querySelector("h1")).style.background = color;
+	msg.textContent = "Correct!";
+	document.querySelector("#reset").textContent = "Play Again?";
 }
 
 function selectSolution(){
-	pickedColorIndex = Math.floor((Math.random())*6);
+	pickedColorIndex = Math.floor((Math.random())*numSquares);
 	pickedColor = colors[pickedColorIndex];
 	colorDisplay.textContent = pickedColor;
 }
