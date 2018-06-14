8122b971e133513f4aa1ad7a738f3974ef274243
Prarthit Mehra <32339911+prarthit@users.noreply.github.com>
2018-06-14 17:59:05 +0530

    new colors functionality added

--- app.js
+++ app.js
@@ -7,18 +7,42 @@ var squares = document.querySelectorAll(".square");
 
 function initialize(){
 	setUpSquares();
+	setUpModebuttons();
 	selectSolution();
 }
 
+function setUpModebuttons(){
+	var reset = document.querySelector("#reset");
+	reset.addEventListener("click",setUpSquares);
+	var modes = document.querySelectorAll(".mode");
+	for(var i=0; i<modes.length; i++){
+		modes[i].addEventListener("click",function(){
+			modes[0].classList.remove("selected");
+			modes[1].classList.remove("selected");
+			this.classList.add("selected");
+		});		
+	}
+}
+
 function setUpSquares(){
+	colors = [];
 	for(var i=0; i<squares.length; i++){
 		colors.push(generateRandomColor());
 		squares[i].style.background = colors[i];
 		squares[i].addEventListener("click",function(){
 			if(this.style.background === pickedColor)
+				changecolor(pickedColor);
+			else
 				this.style.background = "#232323";
 		});
 	};
+}
+
+function changecolor(color){
+	for(var i=0; i<squares.length; i++)
+	{
+		squares[i].style.background = color;
+	}
 }
 
 function selectSolution(){
